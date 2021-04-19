1281b97a5d14b83ba56fb5ce6a3bc22d606e577c
Coco Puff <fahrija_wyroski@live.com>
2021-04-19 12:56:45 -0500

    generate HTML file

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// request to use fs 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js'); //import function from page-template
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+const profileDataArgs = process.argv.slice(2);
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
-};
-printProfileData(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('================');
+
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// };
+// printProfileData(profileDataArgs);
+
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output');
+});
