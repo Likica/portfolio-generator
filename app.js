d78b034ffc8769bd399b49e77d28c5837167d324
Coco Puff <fahrija_wyroski@live.com>
2021-04-19 18:10:12 -0500

    questions and answers array for userinput

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const inquirer = require('inquirer');
 // request to use fs
 // const fs = require('fs');
+const inquirer = require('inquirer');
 // const generatePage = require('./src/page-template.js'); //import function from page-template
 
 // const pageHTML = generatePage(name, github);
@@ -10,12 +10,85 @@ const inquirer = require('inquirer');
 
 //     console.log('Portfolio complete! Check out index.html to see the output');
 // });
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([ //inquire.prompt is the promise
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your Github Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+};
+
+const promptProject = portfolioData => {
+    console.log(`
+    =============
+    Add a New Project
+    ==============
+    `);
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the Project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
     ])
-    .then(answers => console.log(answers));
+        .then(projectData => {
+            portfolioData.projects.push(projectData);
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+        });
+};
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
+
+// promptUser().then(answers => console.log(answers)); // .then method appended to the function call will return a Promise whereas we add whatever we wish to take place after the Promise is resolved/
