fe4e8c3122705c3342f4ffde33bc7294229a0ff0
Coco Puff <fahrija_wyroski@live.com>
2021-04-19 17:06:35 -0500

    inquirer added' q q c
    q
    Q

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-// request to use fs 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js'); //import function from page-template
+const inquirer = require('inquirer');
+// request to use fs
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js'); //import function from page-template
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('================');
-
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-
-// };
-// printProfileData(profileDataArgs);
-
-// console.log(name, github);
-// console.log(generatePage(name, github));
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output');
+// });
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
